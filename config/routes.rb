aba8cbbdd669c61f6abe8ceae6be49f44e936afc
vaha <iliya.vahrushev@gmail.com>
2015-05-23 23:19:35 +0500

    basic functionality for regex-defined answers

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
+  post 'questions/:id' => 'questions#show'
+  
   resources :questions
   resources :answers
   resources :progresses
