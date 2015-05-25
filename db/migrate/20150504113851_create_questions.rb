78586a79d93e1ac3015ff3f054986e3bbe6383ff
vaha <iliya.vahrushev@gmail.com>
2015-05-25 23:20:26 +0500

    fix missed question migration

--- db/migrate/20150504113851_create_questions.rb
+++ db/migrate/20150504113851_create_questions.rb
@@ -3,6 +3,7 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.integer :level
       t.text :content
+	  t.string :qtype
 
       t.timestamps null: false
     end
