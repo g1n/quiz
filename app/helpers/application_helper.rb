c87483c683a91aca74c39351d036798f304b7db6
vaha <iliya.vahrushev@gmail.com>
2015-05-25 18:57:30 +0500

    add Metro UI CSS library & helpers; add 3rd level

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+
+	def metro_check_box(name, value, text)
+		str = "<label class=\"input-control checkbox small-check\">"
+		str += check_box_tag(name, value)
+		str += "<span class=\"check\"></span>"\
+			   "<span class=\"caption\">#{text}</span>"\
+			   "</label>"
+		str.html_safe
+	end
+	
+	def metro_radio_button(name, value, text)
+		str = "<label class=\"input-control radio small-check\">"
+		str += radio_button_tag(name, value)
+		str += "<span class=\"check\"></span>"\
+			   "<span class=\"caption\">#{text}</span>"\
+			   "</label>"
+		str.html_safe
+	end
+	
+	def metro_text_field(name)
+		str = "<div class=\"input-control text\">"
+		str += text_field_tag(name)
+		str += "</div>"
+		str.html_safe
+	end
 end
